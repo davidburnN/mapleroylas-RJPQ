99c951311c805a903eb3ddc923a1589d9d232ab3
davidburnN <qwe1029123@gmail.com>
2026-06-21 23:52:29 +0800

    Configure Firebase for party sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // 部署前請改成你的 Firebase 設定（詳見 README.md）
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef",
+  apiKey: "AIzaSyCujE2TM4cY_S0VytmRAVI1TxtO3RF0P_g",
+  authDomain: "mapleroyals-rjpq.firebaseapp.com",
+  databaseURL: "https://mapleroyals-rjpq-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mapleroyals-rjpq",
+  storageBucket: "mapleroyals-rjpq.firebasestorage.app",
+  messagingSenderId: "379520528752",
+  appId: "1:379520528752:web:19049cd88fbbd5edab1fb5",
+  measurementId: "G-EX5YMGSB1K"
 };
